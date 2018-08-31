de42fb8261877421271829176e318a3eb0798e32
Khôi Tran <tran@puzzle.ch>
2018-08-31 20:40:44 +0200

    QuitAction janitoring

--- src/main/kotlin/ch/compile/blitzremote/actions/QuitAction.kt
+++ src/main/kotlin/ch/compile/blitzremote/actions/QuitAction.kt
@@ -1,11 +1,9 @@
 package ch.compile.blitzremote.actions
 
-import ch.compile.blitzremote.components.BlitzTerminal
-import ch.compile.blitzremote.components.TabbedSSHPanel
 import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
 
-class QuitAction() : AbstractAction("Quit") {
+class QuitAction : AbstractAction("Quit") {
     override fun actionPerformed(p0: ActionEvent?) {
         System.exit(0)
     }
