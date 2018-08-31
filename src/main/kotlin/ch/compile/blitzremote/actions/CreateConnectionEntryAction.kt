f538446458c63f620779df88e16e79731d187399
Khôi Tran <tran@puzzle.ch>
2018-08-31 20:37:11 +0200

    Logging in node creations

--- src/main/kotlin/ch/compile/blitzremote/actions/CreateConnectionEntryAction.kt
+++ src/main/kotlin/ch/compile/blitzremote/actions/CreateConnectionEntryAction.kt
@@ -3,11 +3,17 @@ package ch.compile.blitzremote.actions
 import ch.compile.blitzremote.model.ConnectionEntryTreeNode
 import ch.compile.blitzremote.model.ConnectionFolderTreeNode
 import ch.compile.blitzremote.model.ConnectionModel
+import org.slf4j.LoggerFactory
 import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
 
 class CreateConnectionEntryAction(private val connectionFolderTreeNode: ConnectionFolderTreeNode) : AbstractAction("New connection...") {
+    companion object {
+        val LOG = LoggerFactory.getLogger(this::class.java)!!
+    }
+
     override fun actionPerformed(p0: ActionEvent?) {
+        LOG.info("New ConnectionEntry created (with parent = ${connectionFolderTreeNode.userObject})")
         ConnectionModel.insertNodeInto(ConnectionEntryTreeNode("New connection"), connectionFolderTreeNode, connectionFolderTreeNode.childCount)
     }
 }
--- src/main/kotlin/ch/compile/blitzremote/actions/CreateFolderAction.kt
+++ src/main/kotlin/ch/compile/blitzremote/actions/CreateFolderAction.kt
@@ -1,13 +1,18 @@
 package ch.compile.blitzremote.actions
 
-import ch.compile.blitzremote.model.ConnectionFolder
 import ch.compile.blitzremote.model.ConnectionFolderTreeNode
 import ch.compile.blitzremote.model.ConnectionModel
+import org.slf4j.LoggerFactory
 import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
 
 class CreateFolderAction(private val connectionFolderTreeNode: ConnectionFolderTreeNode) : AbstractAction("New folder...") {
+    companion object {
+        val LOG = LoggerFactory.getLogger(this::class.java)!!
+    }
+
     override fun actionPerformed(p0: ActionEvent?) {
+        CreateConnectionEntryAction.LOG.info("New CreateFolderAction created (with parent = ${connectionFolderTreeNode.userObject})")
         ConnectionModel.insertNodeInto(ConnectionFolderTreeNode("New Folder"), connectionFolderTreeNode, connectionFolderTreeNode.childCount)
     }
 }
