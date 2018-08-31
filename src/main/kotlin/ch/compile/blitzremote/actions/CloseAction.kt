3d4906209e575d566cb8e99c01abfbc1fffebee2
Khôi Tran <tran@puzzle.ch>
2018-08-31 20:35:16 +0200

    Logging on CloseAction

--- src/main/kotlin/ch/compile/blitzremote/actions/CloseAction.kt
+++ src/main/kotlin/ch/compile/blitzremote/actions/CloseAction.kt
@@ -1,15 +1,15 @@
 package ch.compile.blitzremote.actions
 
 import ch.compile.blitzremote.components.BlitzTerminal
-import ch.compile.blitzremote.components.TabbedSSHPanel
 import org.slf4j.LoggerFactory
 import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
 
-open class CloseAction(private val terminal:BlitzTerminal) : AbstractAction("Close...") {
+open class CloseAction(private val terminal: BlitzTerminal) : AbstractAction("Close...") {
     companion object {
-        val LOG = LoggerFactory.getLogger(this::class.java)
+        val LOG = LoggerFactory.getLogger(this::class.java)!!
     }
+
     override fun actionPerformed(p0: ActionEvent?) {
         LOG.info("Closing terminal ${terminal.connectionEntry.name} (${terminal.connectionEntry.hostname})...")
         terminal.close()
