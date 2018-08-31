9e5b9425d52be84bccf0f45548bd832a044812ff
Khôi Tran <tran@puzzle.ch>
2018-08-31 20:35:53 +0200

    Cleaned up ConnectAction

--- src/main/kotlin/ch/compile/blitzremote/actions/ConnectAction.kt
+++ src/main/kotlin/ch/compile/blitzremote/actions/ConnectAction.kt
@@ -2,17 +2,10 @@ package ch.compile.blitzremote.actions
 
 import ch.compile.blitzremote.components.BlitzTerminal
 import ch.compile.blitzremote.components.TabbedSSHPanel
-import ch.compile.blitzremote.helpers.BlitzTtyShellConnector
-import ch.compile.blitzremote.helpers.SessionAvailableListener
 import ch.compile.blitzremote.model.ConnectionEntry
-import ch.compile.blitzremote.settings.BlitzRemoteSettingsProvider
-import com.jcraft.jsch.JSchException
-import com.jcraft.jsch.Session
-import com.jediterm.ssh.jsch.JSchShellTtyConnector
 import org.slf4j.LoggerFactory
 import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
-import kotlin.concurrent.thread
 
 class ConnectAction(private val connectionEntry: ConnectionEntry) : AbstractAction("Connect...") {
     companion object {
@@ -24,8 +17,6 @@ class ConnectAction(private val connectionEntry: ConnectionEntry) : AbstractActi
         val terminal = BlitzTerminal(connectionEntry)
 
         TabbedSSHPanel.add(terminal)
-
-
     }
 
 }
