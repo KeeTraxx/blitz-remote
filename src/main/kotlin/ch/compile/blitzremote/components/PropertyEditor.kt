76aa432d2929956f0af5ffe0b21ce311b30537a0
Khôi Tran <kt@compile.ch>
2018-09-01 22:00:55 +0200

    Property name column is now bold

--- src/main/kotlin/ch/compile/blitzremote/components/PropertyEditor.kt
+++ src/main/kotlin/ch/compile/blitzremote/components/PropertyEditor.kt
@@ -2,7 +2,9 @@ package ch.compile.blitzremote.components
 
 import ch.compile.blitzremote.helpers.UseCellRenderer
 import ch.compile.blitzremote.model.AbstractBlitzTreeNode
+import java.awt.Font
 import javax.swing.JTable
+import javax.swing.table.DefaultTableCellRenderer
 import javax.swing.table.TableCellRenderer
 import kotlin.reflect.full.createInstance
 import kotlin.reflect.full.findAnnotation
@@ -15,7 +17,7 @@ class PropertyEditor(blitzTreeNode: AbstractBlitzTreeNode) : JTable(PropertyEdit
 
     override fun getCellRenderer(row: Int, column: Int): TableCellRenderer {
         if (column == 0) {
-            return super.getCellRenderer(row, column)
+            return BoldCellRenderer
         }
         val m = this.model as PropertyEditorTableModel
         val prop = m.getPropertyTypeAt(row)
@@ -26,5 +28,11 @@ class PropertyEditor(blitzTreeNode: AbstractBlitzTreeNode) : JTable(PropertyEdit
             super.getCellRenderer(row, column)
         }
 
+    }
+
+    object BoldCellRenderer : DefaultTableCellRenderer.UIResource() {
+        override fun setFont(font: Font) {
+            super.setFont(font.deriveFont(Font.BOLD))
+        }
     }
 }
